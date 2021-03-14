2e24dbc8e59e17bed0ec22038fe467bc31ec656f
Chandrakant Khandelwal <chandrakant.khandelwal@intel.com>
2021-03-14 17:29:24 +0530

    Updated Readme and build documentation

--- build_ocm.sh
+++ build_ocm.sh
@@ -9,25 +9,27 @@
 # Check if Tensorflow source directory is provided as input, otheriwse use default installation path
 if [ -z "$1" ]
   then
-    TF_SRC_DIR="$(pwd)/setup/tensorflow/"
+    echo "--- Tensorflow source directory path is not provided ---"
+    exit
 else
     TF_SRC_DIR=$1
-fi
-
-# Check the input as well as default path, and if both of them doesn't exists then exit the script
-if [ -d  ${TF_SRC_DIR} ]
-then
-    echo "Tensorflow source directory is - ${TF_SRC_DIR}"
-    echo "Activating the virtual environment"
-    source "$(pwd)/setup/env/bin/activate"
-else
-    echo "Error: Default Tensorflow source directory doesn't exists"
-    exit 1
+    if [ ! -d  ${TF_SRC_DIR}/tensorflow ]
+    then
+      echo "Tensorflow sourc code doesn't exists at the ${TF_SRC_DIR} "
+      exit
+    fi
 fi
 
 printf "\n---- Starting the OCM Build ------ \n"
 mkdir build
 cd build
-cmake .. -DTF_SRC_DIR=${TF_SRC_DIR} -DFLOAT16_SUPPORT=ON
+cmake .. -DTF_SRC_DIR=${TF_SRC_DIR}/tensorflow -DFLOAT16_SUPPORT=ON
 # the C library should be present at following path ${TF_SRC_DIR}/bazel-bin/tensorflow/libtensorflow_cc.so.2`
 make
+
+if [ -f  ov_ocm ]
+then
+  echo "--- Build Complete ---"
+else
+  echo "--- Build Failed ---"
+fi
