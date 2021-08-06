1433af922e5ca9a426301b1646a19a4b27e8fe9b
Dong <junfeng.dong@intel.com>
2021-08-05 18:40:36 -0700

    Control export variables.

--- include/ocm_nodes_checker.h
+++ include/ocm_nodes_checker.h
@@ -117,7 +117,7 @@ public:
  * which are ideally should be used by user to check which nodes of the graph can
  * run on OpenVINO backends
  */
-class FrameworkNodesChecker{
+OCM_EXPORT class FrameworkNodesChecker{
 public:
   /**
    * OCM Status
