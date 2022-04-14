a4b2874ee6afdd851045de57039ec14528b9e162
Suryaprakash Shanmugam <suryaprakash.shanmugam@intel.com>
2022-04-14 21:58:02 +0530

    Fix debug log message

--- OCM/ocm_nodes_checker.cc
+++ OCM/ocm_nodes_checker.cc
@@ -49,7 +49,7 @@ FrameworkNodesChecker::FrameworkNodesChecker(Framework_Names fw,
    if ((ov_version != "2021.1") && (ov_version != "2021.2") &&
       (ov_version != "2021.3") &&  (ov_version != "2021.4") &&
       (ov_version != "2022.1.0")) {
-    OCM_LOG(3) << "Invalid OpenVINO version - " << device_id
+    OCM_LOG(3) << "Invalid OpenVINO version - " << ov_version
                << ". Allowed options are 2021.1, 2021.2, 2021.3, 2021.4, 2022.1.0" << std::endl;
     ocm_status = OCMStatus::INVALID_OV_VERSION;
     return;
